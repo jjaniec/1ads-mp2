b2f27df14c9ba1f62a3490cf63c222046c582deb
jjaniec <jjaniec@firemail.cc>
2016-12-21 20:33:40 +0100

    fix 3.1.2 + add ft_display_board(3.1.3)

--- src/basis.py
+++ src/basis.py
@@ -9,18 +9,34 @@ def ft_generate_board(n, tuple_):
     """Generate game board, obviously"""
     i = 0;
     j = 0;
-    arr = [[0] * n] * n;
+    arr = [[0] * n for _ in range(n)];
 
     while (j < n):
         while (i < n):
             #arr[j][i] = ft_3_1_1(tuple_);
             arr[j][i] = ft_randint();
             i += 1;
-        j += 1
+        j += 1;
         i = 0;
 
-    #print(arr);
-    return (arr)
+    print(arr);
+    return (arr);
 
-ft_generate_board(int(sys.argv[1]), (0,0,0));
+def ft_display_board(arr, size):
+    """Display our game board"""
+    i = 0;
+    j = 0;
+
+    while (j < size):
+        while (i < size):
+            end_ = " ";
+            if ((i + 1) == size):
+                end_ = "";
+            print(str(arr[j][i]), end=end_);
+            i += 1;
+        print();
+        i = 0;
+        j += 1;
+
+ft_display_board(ft_generate_board(int(sys.argv[1]), (0,0,0)), int(sys.argv[1]));
 #ft_generate_board(5, (0,0,0));
