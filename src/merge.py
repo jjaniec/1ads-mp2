65336563fd18953109b3e47a0f0fcf06d266e1f1
jjaniec <jjaniec@firemail.cc>
2017-01-09 18:02:31 +0100

    rm debug functions[0]&&[1] + begin [2]

--- src/merge.py
+++ src/merge.py
@@ -8,33 +8,47 @@ def     ft_appendable_in_arr(arr, coords, coordstoadd):
 def     ft_get_adj_cells(n, arr, coords, li):
     coordstocheck = [(-1, 0), (1, 0), (0, -1), (0, 1)];
     i = 0;
-    print(arr);
     while (i != 4):
-        print(coordstocheck[i]);
         x = coords[1] + coordstocheck[i][1];
         y = coords[0] + coordstocheck[i][0];
-        print("check (" + str(y) + ", " + str(x) + ")")
         if (x >= 0 and x < n and y >= 0 and y < n):
             if (ft_appendable_in_arr(arr, coords, (y, x)) == 0):
-                print("0");
                 li.append((y, x));
         i += 1;
-    print(li);
 
 def     ft_change_cells_vals(n, arr, li):
     i = 1;
     arr[li[0][0]][li[0][1]] += 1;
     while (i < len(li)):
         arr[li[i][0]][li[i][1]] = 0;
-        print(li[i])
-        print(arr)
         i += 1;
-    print(li);
-    print(arr);
+
+def     ft_fill_zero(n, arr, coords):
+    i = 0;
+    while ((coords[0] - i - 1) > -1):
+        arr[coords[0] - i][coords[1]] = arr[coords[0] - 1 - i][coords[1]];
+        i += 1;
+    arr[coords[0] - i][coords[1]] = 666;
+
+def     ft_fill_cells(n, arr, tupl):
+    i = 0;
+    j = 0;
+    while (i < n):
+        while (j < n):
+            print("run")
+            print(str(arr[i][j]))
+            if (arr[i][j] == 0):
+                print("found0!")
+                ft_fill_zero(n, arr, (i, j));
+            j += 1
+        i += 1
+        print("strend");
+        j = 0
+
 
 tupl = (2,2);
 li = [tupl];
-arr = [[8,8,2,8,1],[8,2,8,8,8],[4,8,8,8,3],[4,2,8,8,4],[4,2,8,1,6]];
+arr = [[8,8,2,8,1],[8,2,8,8,8],[4,8,0,8,3],[4,2,8,8,4],[4,2,8,1,6]];
 ft_get_adj_cells(5, arr, tupl, li);
 ft_change_cells_vals(5, arr, li);
-
+ft_fill_cells(5, arr, tupl);
