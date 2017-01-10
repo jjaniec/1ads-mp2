52b6530e3ab41c234e27313a6d9a70521c7971a8
jjaniec <jjaniec@firemail.cc>
2017-01-10 16:16:48 +0100

    add comment1

--- src/merge.py
+++ src/merge.py
@@ -1,16 +1,21 @@
 import sys
-from typing import Tuple, List
+from typing import Tuple, List, intArray, TupleArray
 from basis import *
 
 #3.3[0]
-def     get_appendable_in_arr(arr, coords, coordstoadd):
+def     get_appendable_in_arr(arr: Board, coords: Tuple[int, int], coordstoadd: Tuple[int, int]) -> int:
+    """Tell if an element in Board should be added in the TupleArray returned by
+    get_adj_cells function"""
+
     if (arr[coords[0]][coords[1]] == arr[coordstoadd[0]][coordstoadd[1]]):
         return (0);
     return (1);
 
-def     get_adj_cells(n, arr, coords, li):
+def     get_adj_cells(n: int, arr: Board, coords: Tuple[int, int], li: TupleArray) -> void:
+    """Return an array of tuple of adjacent cells content equal of the given one"""
     coordstocheck = [(-1, 0), (1, 0), (0, -1), (0, 1)];
     i = 0;
+
     while (i != 4):
         x = coords[1] + coordstocheck[i][1];
         y = coords[0] + coordstocheck[i][0];
@@ -19,23 +24,29 @@ def     get_adj_cells(n, arr, coords, li):
                 li.append((y, x));
         i += 1;
 #3.3[1]
-def     change_cells_values(n, arr, li):
+def     change_cells_values(n: int, arr: Board, li: Tuple[Tuple, Tuple]):
+    """Set += 1 on arr[li[0][0]][li[0][1]] and set other coords to 0"""
     i = 1;
+
     arr[li[0][0]][li[0][1]] += 1;
     while (i < len(li)):
         arr[li[i][0]][li[i][1]] = 0;
         i += 1;
 #3.3[2]
-def     fill_zero(n, arr, coords):
+def     fill_zero(n: int, arr: Board, coords: Tuple[int, int]) -> void:
+    """Change zero by random value returned by generate_cell function"""
     i = 0;
+
     while ((coords[0] - i - 1) > -1):
         arr[coords[0] - i][coords[1]] = arr[coords[0] - 1 - i][coords[1]];
         i += 1;
     arr[coords[0] - i][coords[1]] = generate_cell((0.2,0.3,0.4));
 
-def     remove_zeros(n, arr, tupl):
+def     remove_zeros(n: int, arr: Board, tupl: Tuple[int, int]):
+    """Search for 0s to call fill_zero"""
     i = 0;
     j = 0;
+
     while (i < n):
         while (j < n):
             if (arr[i][j] == 0):
@@ -43,7 +54,6 @@ def     remove_zeros(n, arr, tupl):
             j += 1
         i += 1
         j = 0
-
 
 tupl = (2,2);
 li = [tupl];
