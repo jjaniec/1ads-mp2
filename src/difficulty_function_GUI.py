a36f5485dc094ce613be0dad0f40b282bac27826
jjaniec <jjaniec@firemail.cc>
2017-01-14 22:16:23 +0100

    now returning boardsize

--- src/difficulty_function_GUI.py
+++ src/difficulty_function_GUI.py
@@ -4,11 +4,7 @@ from pygame.locals import *
 WINDOW_SIZE = (500, 500)
 
 def     get_dificulty_GUI(rectspositions, rectssize):
-    print(rectspositions[0])
-    difficulty = (1, 1, 1)
-    EASY = (0.1, 0.2, 0.3)
-    MEDIUM = (0.2, 0.3, 0.4)
-    HARD = (0.3, 0.4, 0.5)
+    GREY = (165, 165, 165)
 
     rect1 = pygame.Rect(rectspositions[0], rectssize)
     rect2 = pygame.Rect(rectspositions[1], rectssize)
@@ -17,9 +13,9 @@ def     get_dificulty_GUI(rectspositions, rectssize):
     rect2_surface = pygame.Surface(rect2.size)
     rect3_surface = pygame.Surface(rect3.size)
     font = pygame.font.SysFont('freesans', 36)
-    rect1_text = font.render("EASY", True, (0, 0, 0))
-    rect2_text = font.render("MEDIUM", True, (0, 0, 0))
-    rect3_text = font.render("HARD", True, (0, 0, 0))
+    rect1_text = font.render("4 x 4", True, (0, 0, 0))
+    rect2_text = font.render("5 x 5", True, (0, 0, 0))
+    rect3_text = font.render("6 x 6", True, (0, 0, 0))
     rect1_textrect = rect1_text.get_rect()
     rect1_textrect.centerx = rect1_surface.get_rect().centerx
     rect1_textrect.centery = rect1_surface.get_rect().centery
@@ -29,21 +25,21 @@ def     get_dificulty_GUI(rectspositions, rectssize):
     rect3_textrect = rect3_text.get_rect()
     rect3_textrect.centerx = rect3_surface.get_rect().centerx
     rect3_textrect.centery = rect3_surface.get_rect().centery
-    rect1_surface.fill((0, 255, 0))
-    rect2_surface.fill((255, 255, 0))
-    rect3_surface.fill((255, 0, 0))
+    rect1_surface.fill(GREY)
+    rect2_surface.fill(GREY)
+    rect3_surface.fill(GREY)
 
-    while difficulty == (1, 1, 1):
+    while 1:
         for e in pygame.event.get():
             if e.type == pygame.QUIT:
                 quit()
             if e.type == MOUSEBUTTONUP and e.button == 1:
                 if rect1.collidepoint(e.pos):
-                    difficulty = EASY
+                    return (4)
                 if rect2.collidepoint(e.pos):
-                    difficulty = MEDIUM
+                    return (5)
                 if rect3.collidepoint(e.pos):
-                    difficulty = HARD
+                    return (6)
 
         window.fill((0, 0, 0)) #background
         window.blit(rect1_surface, rect1)
