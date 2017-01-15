b3820d53496d7e616f047409adbc8942a7a4e15d
jjaniec <jjaniec@firemail.cc>
2017-01-15 13:12:03 +0100

    begin pylint

--- src/get_boardsize_GUI.py
+++ src/get_boardsize_GUI.py
@@ -1,10 +1,14 @@
+from typing import List
 import pygame
 from pygame.locals import *
 
-WINDOW_SIZE = (500, 500)
+Tuple = List[int]
 
-def get_boardsize_GUI(rectpositions, rectssize):
-    GREY = (165, 165, 165)
+WINDOW_SIZE = (400, 500)
+
+def get_boardsize_GUI(rectpositions: List[Tuple], rectssize: Tuple) -> int:
+    """Asks the player the dimensions of the board to chose"""
+    grey = (165, 165, 165)
     font = pygame.font.SysFont('freesans', 36)
     rects = [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]]
     #rectangles / surfaces / texts renders / texts rectangles
@@ -15,21 +19,21 @@ def get_boardsize_GUI(rectpositions, rectssize):
         rects[3][i] = rects[2][i].get_rect()
         rects[3][i].centerx = rects[1][i].get_rect().centerx
         rects[3][i].centery = rects[1][i].get_rect().centery
-        rects[1][i].fill(GREY)
+        rects[1][i].fill(grey)
     while 1:
-        for e in pygame.event.get():
-            if e.type == pygame.QUIT:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
                 quit()
-            if e.type == MOUSEBUTTONUP and e.button == 1:
+            if event.type == MOUSEBUTTONUP and event.button == 1:
                 for i in range(len(rects[0])):
-                    if rects[0][i].collidepoint(e.pos):
-                        return (i + 4)
-        window.fill((0, 0, 0))
+                    if rects[0][i].collidepoint(event.pos):
+                        return i + 4
+        WINDOW.fill((0, 0, 0))
         for i in range(len(rects[0])):
-            window.blit(rects[1][i], rects[0][i])
+            WINDOW.blit(rects[1][i], rects[0][i])
             rects[1][i].blit(rects[2][i], rects[3][i])
         pygame.display.flip()
 
 pygame.init()
-window = pygame.display.set_mode(WINDOW_SIZE)
+WINDOW = pygame.display.set_mode(WINDOW_SIZE)
 print(get_boardsize_GUI(((100, 100), (100, 200), (100, 300)), (200, 90)))
